4264d8a44f7c0053dad768420f9470daef851411
Nikolai Maximov <kolkamaximov@gmail.com>
2022-09-22 23:25:39 +0300

    update model

--- SuperHeroes/Models/Hero.swift
+++ SuperHeroes/Models/Hero.swift
@@ -10,10 +10,17 @@ import Foundation
 // MARK: - WelcomeElement
 struct Hero: Codable {
     let name: String
+    let powerstats: Powerstats
     let images: Images
 }
 
 // MARK: - Images
 struct Images: Codable {
     let xs, sm, md, lg: String
+}
+
+// MARK: - Powerstats
+struct Powerstats: Codable {
+    let intelligence, strength, speed, durability: Int
+    let power, combat: Int
 }
