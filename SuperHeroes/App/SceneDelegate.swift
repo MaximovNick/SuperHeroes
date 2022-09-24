d106c1eb2b0b97e03a4179b15e5884b6ddd78a5f
Nikolai Maximov <kolkamaximov@gmail.com>
2022-09-24 21:02:59 +0300

    change rootVC

--- SuperHeroes/App/SceneDelegate.swift
+++ SuperHeroes/App/SceneDelegate.swift
@@ -16,7 +16,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         
         window = UIWindow(windowScene: windowScene)
-        let navigationVC = UINavigationController(rootViewController: HeroesViewController())
+        let navigationVC = UINavigationController(rootViewController: HeroViewController())
         window?.rootViewController = navigationVC
         window?.makeKeyAndVisible()
     }
