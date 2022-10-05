708712da209d9dc5b643f2700b628c5056f4cb7b
Nikolai Maximov <kolkamaximov@gmail.com>
2022-10-05 20:50:06 +0300

    change model

--- SuperHeroes/Models/Hero.swift
+++ SuperHeroes/Models/Hero.swift
@@ -8,16 +8,29 @@
 import Foundation
 
 // MARK: - WelcomeElement
-struct Hero: Codable {
-    let name: String
-    let biography: Biography
+struct Superhero: Codable {
+    let id: Int
+    let name, slug: String
     let powerstats: Powerstats
+    let appearance: Appearance
+    let biography: Biography
+    let work: Work
+    let connections: Connections
     let images: Images
 }
 
-// MARK: - Images
-struct Images: Codable {
-    let xs, sm, md, lg: String
+// MARK: - Appearance
+struct Appearance: Codable {
+    let gender: Gender
+    let race: String?
+    let height, weight: [String]
+    let eyeColor, hairColor: String
+}
+
+enum Gender: String, Codable {
+    case empty = "-"
+    case female = "Female"
+    case male = "Male"
 }
 
 // MARK: - Biography
@@ -26,10 +39,33 @@ struct Biography: Codable {
     let aliases: [String]
     let placeOfBirth, firstAppearance: String
     let publisher: String?
+    let alignment: Alignment
+}
+
+enum Alignment: String, Codable {
+    case bad = "bad"
+    case empty = "-"
+    case good = "good"
+    case neutral = "neutral"
+}
+
+// MARK: - Connections
+struct Connections: Codable {
+    let groupAffiliation, relatives: String
+}
+
+// MARK: - Images
+struct Images: Codable {
+    let xs, sm, md, lg: String
 }
 
 // MARK: - Powerstats
 struct Powerstats: Codable {
     let intelligence, strength, speed, durability: Int
     let power, combat: Int
+}
+
+// MARK: - Work
+struct Work: Codable {
+    let occupation, base: String
 }
